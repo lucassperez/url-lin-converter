7384d963cba4e3f28d3f959da41e8eb457dc1688
Lucas Perez <lucastperez@gmail.com>
2021-10-31 01:13:39 -0300

    Create urlToLin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import urlToLin from './lib/urlToLin';
 
 function App() {
+  const url = 'https://www.bridgebase.com/tools/handviewer.html?lin=st||pn|Periiz,jeank64,Damares%20C,tbattaggia|md|4SAQ7HK6DKT5CKJ543,SK65HA95DJ862C876,S942HT83DA43CAQT2,SJT83HQJ742DQ97C9|sv|n|rh||ah|Board%202|mb|P|mb|1N|mb|P|mb|3N|mb|P|mb|P|mb|P|pc|D2|pc|D3|pc|DQ|pc|DK|pc|C3|pc|C6|pc|CA|pc|C9|pc|CQ|pc|H2|pc|C4|pc|C7|pc|CT|pc|H4|pc|CK|pc|C8|pc|CJ|pc|D6|pc|C2|pc|D7|pc|C5|pc|H5|pc|H3|pc|D9|pc|DT|pc|D8|pc|D4|pc|S3|pc|D5|pc|DJ|pc|DA|pc|S8|pc|S2|pc|ST|pc|SA|pc|S5|pc|S7|pc|SK|pc|S4|pc|SJ|pc|HA|pc|H8|pc|H7|pc|H6|pc|H9|pc|HT|pc|HJ|pc|HK|pc|SQ|pc|S6|pc|S9|pc|HQ|'
+  const url2 = 'https://www.bridgebase.com/tools/handviewer.html?lin=st||pn|,,,|md|3SKT86542HKDJ82CQ4,SQJ7H932DQ53CAJ76,S3HQJ854DAT97CK85,SA9HAT76DK64CT932|sv|o|rh||ah|Board%200|'
+  const lin = urlToLin(url, 1);
+  const lin2 = urlToLin(url2, 2);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +24,8 @@ function App() {
         >
           Learn React
         </a>
-        <h1>
-        Olá! (:
-        </h1>
+        <p> {lin} </p>
+        <p> {lin2} </p>
       </header>
     </div>
   );
---
+++ src/lib/urlToLin.js
@@ -0,0 +1,66 @@
+/*
+Params:
+  String (url from BBO)
+  Number n (number of board, from 1 and above)
+*/
+function urlToLin (string, n) {
+  const groups = matchGroups(string);
+  return buildLin(groups, n);
+}
+
+/*
+Params:
+  String (url from BBO)
+Returns:
+  Obj { playerNames(string), hands(string), biddingAndPlay(string) }
+*/
+function matchGroups (string) {
+  return string
+           .replaceAll('%20', ' ')
+           .match(/(http.*lin=st)\|\|pn\|(?<playerNames>.*)\|md\|[1-4](?<hands>[2-9AKQJTSHDC]{17},[2-9AKQJTSHDC]{17},[2-9AKQJTSHDC]{17},[2-9AKQJTSHDC]{17}).*Board [0-9]\|*(?<biddingAndPlay>.*)/)
+           .groups;
+}
+
+/*
+Params:
+  Obj { playerNames(string), hands(string), biddingAndPlay(string) }
+  Number n (number of board, from 1 and above)
+Returns:
+  String (formatted lin with calculated vul and dealer by board number)
+*/
+function buildLin ({playerNames: pn, hands, biddingAndPlay: bap}, n) {
+  const d = dealerByIndex(n);
+  const v = vulByIndex(n);
+  return `qx|o${n}|${pn}|md|${d}${hands}|sv|${v}|rh||ah|Board ${n}|${bap}`;
+}
+
+function dealerByIndex (index) {
+  // 1 = south, 2 = west, 3 = north and 4 = east
+  return ((index + 1) % 4) + 1;
+}
+
+function vulByIndex (index) {
+  // o = none, n = north and south, e = east and west, b = all (both)
+  switch (index) {
+    case 1: return 'o';
+    case 2: return 'n';
+    case 3: return 'e';
+    case 4: return 'b';
+    case 5: return 'n';
+    case 6: return 'e';
+    case 7: return 'b';
+    case 8: return 'o';
+    case 9: return 'e';
+    case 10: return 'b';
+    case 11: return 'o';
+    case 12: return 'n';
+    case 13: return 'b';
+    case 14: return 'o';
+    case 15: return 'n';
+    case 16: return 'e';
+    case 0: return 'e'; // board 0 shouldn't be allowed, but who knows
+    default: return vulByIndex(index % 16);
+  }
+}
+
+export default urlToLin;
