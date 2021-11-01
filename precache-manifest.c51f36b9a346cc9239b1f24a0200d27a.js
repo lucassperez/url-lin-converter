faabf095233603d5401de2c066ce798d93c69ced
Lucas Perez <lucastperez@gmail.com>
2021-10-31 23:38:52 -0300

    Updates

---
+++ precache-manifest.c51f36b9a346cc9239b1f24a0200d27a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c704bdec21304faf68cd0dca0e61b1",
+    "url": "/url-lin-converter/index.html"
+  },
+  {
+    "revision": "3a660dadd293ba6875c0",
+    "url": "/url-lin-converter/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "84ca6f50f8ad74846eb5",
+    "url": "/url-lin-converter/static/js/2.a7240cd9.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/url-lin-converter/static/js/2.a7240cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a660dadd293ba6875c0",
+    "url": "/url-lin-converter/static/js/main.1545e321.chunk.js"
+  },
+  {
+    "revision": "641bb44590f654299be7",
+    "url": "/url-lin-converter/static/js/runtime-main.4d56a732.js"
+  }
+]);
