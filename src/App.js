25129fd60f6f665559d52f53397811d5240b6200
Lucas Perez <lucastperez@gmail.com>
2021-10-31 22:06:02 -0300

    Create linToUrl

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import urlToLin from './lib/urlToLin';
+import linToUrl from './lib/linToUrl'
 
 function App() {
-  const url = 'https://www.bridgebase.com/tools/handviewer.html?lin=st||pn|Periiz,jeank64,Damares%20C,tbattaggia|md|4SAQ7HK6DKT5CKJ543,SK65HA95DJ862C876,S942HT83DA43CAQT2,SJT83HQJ742DQ97C9|sv|n|rh||ah|Board%202|mb|P|mb|1N|mb|P|mb|3N|mb|P|mb|P|mb|P|pc|D2|pc|D3|pc|DQ|pc|DK|pc|C3|pc|C6|pc|CA|pc|C9|pc|CQ|pc|H2|pc|C4|pc|C7|pc|CT|pc|H4|pc|CK|pc|C8|pc|CJ|pc|D6|pc|C2|pc|D7|pc|C5|pc|H5|pc|H3|pc|D9|pc|DT|pc|D8|pc|D4|pc|S3|pc|D5|pc|DJ|pc|DA|pc|S8|pc|S2|pc|ST|pc|SA|pc|S5|pc|S7|pc|SK|pc|S4|pc|SJ|pc|HA|pc|H8|pc|H7|pc|H6|pc|H9|pc|HT|pc|HJ|pc|HK|pc|SQ|pc|S6|pc|S9|pc|HQ|'
+  const url = 'https://www.bridgebase.com/tools/handviewer.html?lin=st||pn|Periiz,~Mwest,~Mnorth,~Meast|md|3SA87HAT874DAJTCKT,SQT6HK53DKQ32CQJ2,SJ5HQJD754C976543,SK9432H962D986CA8|sv|n|rh||ah|Board%205|mb|P|mb|P|mb|1N|an|notrump%20opener.%20Could%20have%205M.%20--%202-5%20!C;%202-5%20!D;%202-5%20!H;%202-5%20!S;%2015-17%20HCP;%2018-%20total%20points|mb|P|mb|2N!|an|Minor%20transfer%20--%206+%20!C;%2011-%20HCP;%2012-%20total%20points|mb|P|mb|3C|an|Transfer%20completed%20--%202-5%20!C;%202-5%20!D;%202-5%20!H;%202-5%20!S;%2015-17%20HCP;%2018-%20total%20points|mb|P|mb|P|mb|P|pc|DK|pc|D4|pc|D6|pc|DA|pc|DT|pc|DQ|pc|D5|pc|D9|pc|D2|pc|D7|pc|D8|pc|DJ|pc|CT|pc|CJ|pc|C3|pc|C8|pc|C2|pc|C4|pc|CA|pc|CK|pc|SK|pc|SA|pc|S6|pc|S5|pc|S7|pc|SQ|pc|SJ|pc|S9|pc|CQ|pc|C5|pc|H2|pc|H4|pc|D3|pc|C6|pc|S4|pc|S8|pc|HJ|pc|H9|pc|H7|pc|HK|pc|H5|pc|HQ|pc|H6|pc|HA|mc|7|'
   const url2 = 'https://www.bridgebase.com/tools/handviewer.html?lin=st||pn|,,,|md|3SKT86542HKDJ82CQ4,SQJ7H932DQ53CAJ76,S3HQJ854DAT97CK85,SA9HAT76DK64CT932|sv|o|rh||ah|Board%200|'
   const lin = urlToLin(url, 1);
   const lin2 = urlToLin(url2, 2);
+  const u = linToUrl(lin);
 
   return (
     <div className="App">
@@ -26,6 +28,7 @@ function App() {
         </a>
         <p> {lin} </p>
         <p> {lin2} </p>
+        <a href={u} target='_blank'>link</a>
       </header>
     </div>
   );
---
+++ src/lib/linToUrl.js
@@ -0,0 +1,5 @@
+function linToUrl (string) {
+  return `https://www.bridgebase.com/tools/handviewer.html?lin=st|${string}`;
+}
+
+export default linToUrl;
